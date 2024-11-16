34edbad0f1931a01d549ea5620778969f6e8cd88
karsaibence <karesz333@hotmail.com>
2024-11-16 18:19:08 +0100

    uzh

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,14 +19,23 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('role')->default(3);
             $table->rememberToken();
             $table->timestamps();
         });
 
         User::create([
-            'name' => 'Kovács Gizi', 
-            'email' => 'gizi@teszt.hu', 
-            'password'=>'gizi123']);
+            'name' => 'Kovács Gizi',
+            'email' => 'gizi@teszt.hu',
+            'password' => Hash::make('gizi123')
+        ]);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@teszt.hu',
+            'password' => Hash::make('admin123'),
+            'role' => 0
+        ]);
 
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,25 @@
 <?php
 
-use App\Http\Controllers\LendingController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//bárki által elérhető
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/register',[RegisteredUserController::class, 'store']);
 
-//User végpontok
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+//autentikált útvonal
+Route::middleware(['auth:sanctum'])
+    ->group(function () {
 
-//Lending végpontok
-Route::get('/lendings', [LendingController::class, 'index']);
-Route::get('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'show']);
-Route::post('/lendings', [LendingController::class, 'store']);
-//Route::put('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'update']);
-Route::delete('/lendings/{user_id}/{copy_id}/{start}', [LendingController::class, 'destroy']);
+        // Kijelentkezés útvonal
+        Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+    });
+
+Route::middleware(['auth:sanctum', Admin::class])
+    ->group(function () {
+        //Route::get('/admin/users', [UserController::class, 'index']);
+        Route::apiResource('/admin/users', UserController::class);
+    });
